017f59c7716eb1b9bfaf2715ee2638181beb60b0
marcuusjames <marcuusjames@hotmail.com>
2021-01-25 19:04:42 -0300

    Novas pages

---
+++ js/tabs.js
@@ -0,0 +1,24 @@
+$("#nav-placeholder").append('\
+    <nav class="vis_desktop navbar fixed-top navbar-expand-lg py-md-3" style="background-color: #000000;font-size:18px;height:14%">\
+        <a class="navbar-brand" href="http://andescreation.com/" style="padding-left:2rem;">\
+            <img src="http://andescreation.com/images/logo.png" class="d-inline-block align-top logoimage" alt="">\
+        </a>\
+        <div id="_pages">\
+            <div>\
+                <ul class="nav navbar-nav flex-row justify-content-md-center justify-content-start flex-nowrap">\
+                    <li class="nav-item">\
+                        <a href="http://andescreation.com/about" class="nav-link m-2 menu-item nav-active textmenu">SOBRE</a>\
+                    </li>\
+                    <li class="nav-item">\
+                        <a href="http://andescreation.com/contact" class="nav-link m-2 menu-item textmenu">CONTATO</a>\
+                    </li>\
+                    <li class="nav-item">\
+                        <a href="http://andescreation.com/services" class="nav-link m-2 menu-item textmenu">SERVIÇOS</a>\
+                    </li>\
+                    <li class="nav-item">\
+                        <a href="http://andescreation.com/jobmate" class="nav-link m-2 menu-item textmenu">SEJA NOSSO JOBMATE</a>\
+                    </li>\
+                </ul>\
+            </div>\
+        </div>\
+    </nav>');

c80f7eb3b042e0cccfe156730823ea3151504217
marcuusjames <marcuusjames@hotmail.com>
2021-02-09 00:07:46 -0300

    deploy

--- js/tabs.js
+++ js/tabs.js
@@ -1,5 +1,5 @@
 $("#nav-placeholder").append('\
-    <nav class="vis_desktop navbar fixed-top navbar-expand-lg py-md-3" style="background-color: #000000;font-size:18px;height:14%">\
+    <nav class="vis_desktop navbar fixed-top navbar-expand-lg py-md-3" style="padding-right: 16vw;justify-content: space-between;background-color: #000000;font-size:18px;height:14%">\
         <a class="navbar-brand" href="http://andescreation.com/" style="padding-left:2rem;">\
             <img src="http://andescreation.com/images/logo.png" class="d-inline-block align-top logoimage" alt="">\
         </a>\
@@ -22,3 +22,18 @@ $("#nav-placeholder").append('\
             </div>\
         </div>\
     </nav>');
+
+$("#footer_div").append('\
+<div class="footer-second">\
+    <div style="float: left;">\
+    <p class="m-0">All Right Reserved 2020 - <a href="http://andescreation.com/"\
+        target="_blank">Andes Creation CAPITAL</a></p>\
+    </div>\
+    <div class="redessociais_fot" style="float: right;padding-top:5%">\
+        <p class="m-0">SIGA A GENTE</p>\
+        <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"></i>\
+        <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"></i>\
+        <i class="fab fa-whatsapp fa-lg white-text mr-md-5 mr-3 fa-2x"></i>\
+    </div>\
+</div>\
+')

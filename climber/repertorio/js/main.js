eabf334b0f2e6b221715ed766302336ed2f74232
marcuusjames <marcuusjames@hotmail.com>
2021-07-24 23:33:12 -0300

    rep

--- climber/repertorio/js/main.js
+++ climber/repertorio/js/main.js
@@ -136,3 +136,47 @@ $(document).ready(function(){
         });
 
     })
+
+
+    function updateClimber(){
+        data = {}
+        $(".textfield").each(function() {
+            data[$(this).attr('id')]=$(this).val()
+        })
+        $(".datefield").each(function(){
+            var pattern = /(\d{2})\/(\d{2})\/(\d{4})/;
+            var dt = new Date($(this).val().replace(pattern,'$3-$2-$1'));
+            data[$(this).attr('id')]=dt;
+        })
+        $("#typeclimber").children('div').each(function(){
+        $(this).children('input').each(function(){
+            data['typeclimber'][$(this).attr('id')]=$(this).prop("checked")
+        })})
+        $(".tagsfield").each(function(){
+            data[$(this).attr('id')]={}
+            f = $(this).attr('id')
+            $(this).children('.tagtext').each(function(){
+                if($(this).hasClass('tagclick')){
+                    data[f][$(this).text()]=true
+                } else {
+                    data[f][$(this).text()]=false
+                }
+            })
+        })
+    
+        $(".select2tags").each(function(){
+            data[$(this).attr('id')]=$(this).val()
+        })
+    
+    
+        $('.inputfield').each(function(){
+            data[$(this).attr('id')]=$(this).prop('checked')
+        })
+    
+        $('.radiofield').each(function(){
+            data[$(this).attr('id')]=$('input[name='+$(this).attr('id').slice(0, -1)+']:checked').val();
+        })
+        updateUser(data)
+    
+        console.log(data)
+    }

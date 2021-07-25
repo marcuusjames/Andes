bc8d8a82a427c3504d4ba0d79a5fac832f31225a
marcuusjames <marcuusjames@hotmail.com>
2021-07-25 17:50:27 -0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $('.open_menu').click(function(){
     $(this).parent().find('.ac_menu_div').toggle('slow');
+    $(this).parent().find('.plus_menu').toggle();
+    $(this).parent().find('.minus_menu').toggle();
 })
 
 
